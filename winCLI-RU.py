bb5ae89e873a8c6bad177b7136b4b7a7ae56c1d7
asidsx <106923482+asidsx@users.noreply.github.com>
2024-10-16 18:43:00 +0500

    Update winCLI-RU.py

--- winCLI-RU.py
+++ winCLI-RU.py
@@ -7,7 +7,7 @@ import warnings
 import logging
 import configparser
 import os
-import platform  # Import the platform module to check OS type
+import platform  # Импортируем модуль platform для проверки типа ОС
 
 from requests.packages import urllib3
 
@@ -19,42 +19,62 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-# Путь к конфигурационному файлу для Windows
+# Путь к файлу конфигурации для Windows
 CONFIGFILE = os.path.join(os.environ['APPDATA'], 'satisfactory-cli.ini')
 
 # Глобальная переменная для URL сервера
-SERVER_URL = 'https://localhost:7777/api/v1'  # По умолчанию локальный сервер
+SERVER_URL = 'https://localhost:7777/api/v1'  # Локальный сервер по умолчанию
+
 
 def clear_screen():
-    """Clear the terminal screen."""
+    """Очистить экран терминала."""
     os.system('cls' if platform.system() == 'Windows' else 'clear')
 
+
 def authenticate(password):
-    """Authenticate with the server and retrieve a Bearer token."""
+    """Аутентификация на сервере и получение токена Bearer."""
     response = send_command(None, "PasswordLogin", {"Password": password, "MinimumPrivilegeLevel": "Administrator"})
     if response:
         token_data = response.json()
         return token_data.get("data").get('authenticationToken')
     return None
 
+def get_server_options(token):
+    """Получить и отобразить параметры сервера."""
+    response = send_command(token, "GetServerOptions")
+    if response:
+        options_data = response.json().get("data", {}).get("serverOptions", {})  # Обновите эту строку
+        
+        if not options_data:
+            click.echo("Параметры не найдены в ответе сервера.")
+            return
+
+        click.echo("\nПараметры сервера:\n")
+        for key, value in options_data.items():
+            click.echo(f"  {key}: {value}")
+
+        click.echo("\nЗапрос параметров завершен.")
+    else:
+        click.echo("Ответ от сервера не получен.")
+
 
 def shutdown_server(token):
     response = send_command(token, "Shutdown")
     if response:
-        click.echo("Server Status:")
+        click.echo("Статус сервера:")
         click.echo(json.dumps(response.json(), indent=4))
 
 
 def save_game(token, name):
-    """Save game with the provided name."""
+    """Сохранить игру с заданным именем."""
     response = send_command(token, "SaveGame", {"SaveName": name})
     if response:
-        click.echo("Game saved.")
+        click.echo("Игра сохранена.")
     return
 
 
 def get_server_status(token):
-    """Fetch and display the server status in a formatted way."""
+    """Получить и отобразить статус сервера в формате."""
     response = send_command(token, "QueryServerState")
     if response:
         server_state = response.json().get("data", {}).get("serverGameState", {})
@@ -66,32 +86,30 @@ def get_server_status(token):
         click.echo("\nСтатус сервера:\n")
         click.echo(f"  Активная сессия: {server_state.get('activeSessionName', 'Нет активной сессии')}")
         click.echo(f"  Подключенные игроки: {server_state.get('numConnectedPlayers', 0)} из {server_state.get('playerLimit', 'неизвестно')}")
-        click.echo(f"  Текущий технологический уровень: {server_state.get('techTier', 'неизвестно')}")
+        click.echo(f"  Текущий техуровень: {server_state.get('techTier', 'неизвестно')}")
         click.echo(f"  Активная схема: {server_state.get('activeSchematic', 'неизвестно')}")
-        click.echo(f"  Текущая игровая фаза: {server_state.get('gamePhase', 'неизвестно')}")
         click.echo(f"  Игра запущена: {'Да' if server_state.get('isGameRunning') else 'Нет'}")
-        click.echo(f"  Игра приостановлена: {'Да' if server_state.get('isGamePaused') else 'Нет'}")
-        click.echo(f"  Средняя скорость обновления: {server_state.get('averageTickRate', 'неизвестно')} FPS")
+        click.echo(f"  Игра на паузе: {'Да' if server_state.get('isGamePaused') else 'Нет'}")
+        click.echo(f"  Средняя частота кадров: {server_state.get('averageTickRate', 'неизвестно')} FPS")
         total_duration = server_state.get('totalGameDuration', 0)
 
-        # Преобразование общего времени игры в часы, минуты и секунды
+        # Преобразование общего игрового времени в часы, минуты и секунды
         hours, remainder = divmod(total_duration, 3600)
         minutes, seconds = divmod(remainder, 60)
         click.echo(f"  Общее время игры: {hours}ч {minutes}м {seconds}с")
 
-        click.echo("\nЗапрос статуса завершён.")
-
+        click.echo("\nЗапрос статуса завершен.")
 
 
 def enumerate_sessions(token):
-    """Функция для перечисления сессий и отображения их в удобочитаемом виде"""
+    """Функция для перечисления сессий и отображения их в удобном формате."""
     response = send_command(token, "EnumerateSessions")
     if response:
         sessions_data = response.json().get("data", {}).get("sessions", [])
         current_session_index = response.json().get("data", {}).get("currentSessionIndex", None)
 
         if not sessions_data:
-            click.echo("Нет доступных сессий.")
+            click.echo("Доступных сессий нет.")
             return
 
         click.echo("Доступные сессии:\n")
@@ -101,7 +119,7 @@ def enumerate_sessions(token):
 
             save_headers = session.get("saveHeaders", [])
             if not save_headers:
-                click.echo("  Нет сохранений для этой сессии.")
+                click.echo("  Сохранений для этой сессии нет.")
             else:
                 for save_idx, save in enumerate(save_headers):
                     save_name = save.get("saveName", "Неизвестное сохранение")
@@ -110,7 +128,7 @@ def enumerate_sessions(token):
                     is_modded = "Да" if save.get("isModdedSave", False) else "Нет"
                     build_version = save.get("buildVersion", "Неизвестная версия")
                     
-                    # Преобразование времени игры в часы, минуты и секунды
+                    # Преобразование игрового времени в часы, минуты и секунды
                     hours, remainder = divmod(play_duration, 3600)
                     minutes, seconds = divmod(remainder, 60)
 
@@ -121,10 +139,9 @@ def enumerate_sessions(token):
                     click.echo(f"    Модифицированное сохранение: {is_modded}")
             
             if idx == current_session_index:
-                click.echo(f"  * Это текущая активная сессия")
+                click.echo(f"  * Это текущая активная сессия.")
 
         click.echo("\nПеречисление завершено.")
-
 
 
 def read_config():
@@ -163,46 +180,47 @@ def send_command(token, funcName, data=None):
         response = requests.post(SERVER_URL, headers=headers, verify=False, json=jsonreq)
 
         if response.status_code >= 200 and response.status_code < 300:
-            click.echo(f"Command success: {response.status_code}")
+            click.echo(f"Команда успешно выполнена: {response.status_code}")
             return response
         else:
-            click.echo(f"Failed to execute command: {response.status_code} {response.reason}")
+            click.echo(f"Не удалось выполнить команду: {response.status_code} {response.reason}")
             click.echo(response.text)
             return None
     except requests.exceptions.RequestException as e:
-        click.echo(f"An error occurred: {e}")
+        click.echo(f"Произошла ошибка: {e}")
         raise e
 
 
 def display_menu():
-    """Отображение меню для выбора команды"""
+    """Отобразить меню для выбора команды."""
     click.echo("\nВыберите команду:")
     click.echo("1. Показать статус сервера")
     click.echo("2. Сохранить игру")
     click.echo("3. Выключить сервер")
     click.echo("4. Перечислить сессии")
-    click.echo("5. Выйти\n")
+    click.echo("5. Показать параметры сервера")  # Новый пункт
+    click.echo("6. Выйти\n")  # Обновленный номер пункта
 
 
 @click.command()
-@click.option('--host', 'host', default="localhost:7777", help='Host:port to connect to')
-@click.option('--password', hide_input=True, help='Password for server authentication.')
+@click.option('--host', 'host', default="localhost:7777", help='Хост:порт для подключения')
+@click.option('--password', hide_input=True, help='Пароль для аутентификации на сервере.')
 def cli(host, password):
-    """CLI tool to authenticate and interact with the Satisfactory Dedicated Server API."""
+    """CLI-инструмент для аутентификации и взаимодействия с API выделенного сервера Satisfactory."""
     config = read_config()
     token = config.get("server", "token")
 
     if not token:
-        # Если токен отсутствует, запрашиваем пароль
-        password = click.prompt("Password", hide_input=True)
+        # Если токена нет, запросить пароль
+        password = click.prompt("Пароль", hide_input=True)
 
-        click.echo("Authenticating...")
+        click.echo("Аутентификация...")
         token = authenticate(password)
         if token:
             config['server']['token'] = token
             save_config(config)
         else:
-            click.echo("Authentication failed.")
+            click.echo("Аутентификация не удалась.")
             return
 
     if host:
@@ -211,9 +229,11 @@ def cli(host, password):
 
     # Меню после аутентификации
     while True:
+
         display_menu()
         choice = click.prompt("Введите номер команды", type=int)
-        clear_screen()
+
+        clear_screen()  # Очистить экран перед выполнением команды
 
         if choice == 1:
             get_server_status(token)
@@ -227,6 +247,8 @@ def cli(host, password):
         elif choice == 4:
             enumerate_sessions(token)
         elif choice == 5:
+            get_server_options(token)  # Вызов новой функции
+        elif choice == 6:
             click.echo("Выход из программы.")
             break
         else:
